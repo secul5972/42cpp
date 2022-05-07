e6d9212c60f540677211bb495737f1966559a580
Seungchul Oh <seungcoh@c1r4s1.42seoul.kr>
2022-05-07 17:16:44 +0900

    ex02_base

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,21 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seungcoh <seungcoh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/07 17:15:35 by seungcoh          #+#    #+#             */
-/*   Updated: 2022/05/07 17:16:02 by seungcoh         ###   ########.fr       */
+/*   Created: 2022/05/07 17:15:31 by seungcoh          #+#    #+#             */
+/*   Updated: 2022/05/07 17:16:01 by seungcoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-
-Zombie*	zombieHorde(int N, std::string name)
-{
-	Zombie *tmp = new Zombie[N];
-	for(int i = 0; i < N; i++)
-		tmp[i].setname(name);
-    return tmp;
-}
