ea932f08ad0aa2567c3fd65091ea2c80595467a5
Seungchul Oh <seungcoh@c1r8s1.42seoul.kr>
2022-06-26 16:35:20 +0900

    ex01

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungcoh <seungcoh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/26 14:05:40 by seungcoh          #+#    #+#             */
+/*   Updated: 2022/06/26 14:28:15 by seungcoh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+ #ifndef FIXED_HPP
+ #define FIXED_HPP
+
+class Fixed
+{
+private:
+	int value;
+	static const int frac = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &copy);
+	Fixed& operator=(const Fixed& fixed);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+ #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungcoh <seungcoh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/26 14:22:13 by seungcoh          #+#    #+#             */
+/*   Updated: 2022/06/26 14:37:01 by seungcoh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return 0;
+}
