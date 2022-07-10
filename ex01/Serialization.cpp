2754d9c2a77a693b61157bdb93d661dc6091cdf8
secul5972 <secul5972@naver.com>
2022-07-10 15:10:49 +0900

    ex01

---
+++ ex01/Serialization.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serialization.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: secul5972 <secul5972@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 14:51:56 by secul5972         #+#    #+#             */
+/*   Updated: 2022/07/10 14:52:17 by secul5972        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serialization.hpp"
+
+uintptr_t serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
